4090ccfd218e86ccd4ab8ca3035675c3832526a8
WeCanStart <artemhapilov2004@gmail.com>
2024-02-12 22:43:25 +0300

    1.0

--- app.cpp
+++ app.cpp
@@ -8,6 +8,7 @@
 #include <set>
 #include <cmath>
 #include <Windows.h>
+#include <cstdlib>
 
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
@@ -50,6 +51,7 @@ void InputTextWithHint(const std::string& label, const std::string& hint, std::s
 
 std::set<char> ops = { '+', '-', 'X', '/' };
 int dpi = GetDpiForSystem();
+bool egypt = false;
 
 char intToDigit(const int& i) {
     if (0 <= i && i <= 9) {
@@ -68,6 +70,29 @@ int digitToInt(const char& c) {
     }
 }
 
+const std::string EGYPT_ALPH = "ABCDEF"; // max 1e5 !!!
+std::string decToEgypt(int val) {
+    std::string numb = std::to_string(val);
+    reverse(numb.begin(), numb.end());
+    std::string res = "";
+    for (int i = 0; i < numb.size(); ++i) {
+        for (int j = 0; j < numb[i] - '0'; ++j) {
+            res += EGYPT_ALPH[i];
+        }
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+int egyptToDec(std::string numb) {
+    int res = 0;
+    for (auto& el : numb) {
+        res += pow(10, el - 'A');
+    }
+    return res;
+}
+
+
 void eval(std::string& output, const int& base, const int& accuracy) {
     bool commaAlready = false;
     bool first = true;
@@ -75,6 +100,9 @@ void eval(std::string& output, const int& base, const int& accuracy) {
     long double dAns = 0.f;
     long double dRhs = 0.f;
     long double order = 1.f;
+
+	std::string egyptStrL = "";
+	std::string egyptStrR = "";
 
     char op = 0;
 
@@ -96,56 +124,80 @@ void eval(std::string& output, const int& base, const int& accuracy) {
             order /= base;
         }
         if (first) {
-            dAns *= base;
-            dAns += ('0' <= el && el <= '9') ? (el - '0') : (el - 'A' + 10);
+            if (egypt) {
+                egyptStrL += el;
+            }
+            else {
+                dAns *= base;
+                dAns += ('0' <= el && el <= '9') ? (el - '0') : (el - 'A' + 10);
+            }
         }
         else {
-            dRhs *= base;
-            dRhs += ('0' <= el && el <= '9') ? (el - '0') : (el - 'A' + 10);
+			if (egypt) {
+				egyptStrR += el;
+			}
+			else {
+                dRhs *= base;
+                dRhs += ('0' <= el && el <= '9') ? (el - '0') : (el - 'A' + 10);
+			}
         }
     }
-	dAns += 0.00001f;
-    dRhs *= order;
-    if (op == '*') {
-        dAns *= dRhs;
-    }
-    if (op == '/') {
-        dAns /= dRhs;
-    }
-    if (op == '+') {
-        dAns += dRhs;
-    }
-    if (op == '-') {
-        dAns -= dRhs;
-        dAns = std::abs(dAns);
-    }
-    long double maxPower = 1.f / std::powl(base, accuracy);
-	if (dAns > maxPower) { //looks crazy, but it works
-        while (dAns > maxPower) {
-            maxPower *= base;
+    if (egypt) {
+        if (op == '*') {
+			output = decToEgypt(egyptToDec(egyptStrL) * egyptToDec(egyptStrR));
         }
-        maxPower /= base;
-    }
-    output = "";
-    commaAlready = false;
-    if (maxPower < 0.9) {
-        output += '0';
-        if (accuracy) output += '.';
-        commaAlready = true;
-    }
-    long double dAnsCp = dAns;
-	maxPower = std::max(maxPower, 1. / (long double)base);
-    while (maxPower * 1.5 > 1.f / std::powl(base, accuracy)) {
-        int digit = 0;
-        while (dAnsCp > maxPower) {
-            dAnsCp -= maxPower;
-            ++digit;
+        if (op == '+') {
+			output = decToEgypt(egyptToDec(egyptStrL) + egyptToDec(egyptStrR));
         }
-        maxPower /= base;
-        output += intToDigit(digit);
-        if (maxPower < 0.99 && !commaAlready && accuracy) {
-            output += ".";
+        if (op == '-') {
+			output = decToEgypt(egyptToDec(egyptStrL) - egyptToDec(egyptStrR));
+        }
+    }
+    else {
+        dAns += std::numeric_limits<float>::epsilon();
+        dRhs *= order;
+        if (op == '*') {
+            dAns *= dRhs;
+        }
+        if (op == '/') {
+            if (dRhs == 0) { output = ""; return; }
+            dAns /= dRhs;
+        }
+        if (op == '+') {
+            dAns += dRhs;
+        }
+        if (op == '-') {
+            dAns -= dRhs;
+            dAns = std::abs(dAns);
+        }
+        long double maxPower = 1.f / std::powl(base, accuracy);
+        if (dAns > maxPower) { //looks crazy, but it works
+            while (dAns > maxPower) {
+                maxPower *= base;
+            }
+            maxPower /= base;
+        }
+        output = "";
+        commaAlready = false;
+        if (maxPower < 0.9) {
+            output += '0';
+            if (accuracy) output += '.';
             commaAlready = true;
+        }
+        long double dAnsCp = dAns;
+        maxPower = std::max(maxPower, 1. / (long double)base);
+        while (maxPower * 1.5 > 1.f / std::powl(base, accuracy)) {
+            int digit = 0;
+            while (dAnsCp > maxPower) {
+                dAnsCp -= maxPower;
+                ++digit;
+            }
+            maxPower /= base;
+            output += intToDigit(digit);
+            if (maxPower < 0.99 && !commaAlready && accuracy) {
+                output += ".";
+                commaAlready = true;
+            }
         }
     }
 }
@@ -153,7 +205,8 @@ void eval(std::string& output, const int& base, const int& accuracy) {
 void digitButton(std::string& output, char d, ImVec2 wSize, int base, ImGuiStyle& style) {
     ImVec4 color1 = style.Colors[ImGuiCol_Button];
     ImVec4 color2 = style.Colors[ImGuiCol_Text];
-    if (digitToInt(d) >= base) {
+    if (!egypt && digitToInt(d) >= base ||
+        egypt && (0 <= digitToInt(d) && digitToInt(d) <= 9)) {
         float w1 = style.Colors[ImGuiCol_Button].w;
         float w2 = style.Colors[ImGuiCol_Text].w;
         style.Colors[ImGuiCol_Button] = style.Colors[ImGuiCol_Button] * 0.7f;
@@ -162,21 +215,32 @@ void digitButton(std::string& output, char d, ImVec2 wSize, int base, ImGuiStyle
         style.Colors[ImGuiCol_Button].w = w1;
         style.Colors[ImGuiCol_Text].w = w2;
     }
-    if (ImGui::Button(std::string(1, d).c_str(), ImVec2(9000 / dpi, 9000 / dpi))) {
-        if (digitToInt(d) >= base || ((output == "0" || output.back() == '0' && ops.find(output[output.size() - 2]) != ops.end()) && d == '0')){}
-        else {
-            if (output == "0") {
-                output = "";
-            }
-            output += d;
+    if (ImGui::Button(std::string(1, d).c_str(), ImVec2(dpi / 2, dpi / 2)) &&
+        !(!egypt && (digitToInt(d) >= base ||
+          ((output == "0" || output.back() == '0' && ops.find(output[output.size() - 2]) != ops.end()) && d == '0')) ||
+          egypt && (0 <= digitToInt(d) && digitToInt(d) <= 9))) {
+        if (output == "0") {
+            output = "";
         }
+        output += d;
     }
     style.Colors[ImGuiCol_Button] = color1;
     style.Colors[ImGuiCol_Text] = color2;
 }
 
-void operatorButton(std::string& output, char oper, bool& op, bool& commaAlready, int base, int accuracy, ImVec2 wSize) {
-    if (ImGui::Button(std::string(1, oper).c_str(), ImVec2(9000 / dpi, 9000 / dpi))) {
+void operatorButton(std::string& output, char oper, bool& op, bool& commaAlready, int base, int accuracy, ImVec2 wSize, ImGuiStyle& style) {
+    ImVec4 color1 = style.Colors[ImGuiCol_Button];
+    ImVec4 color2 = style.Colors[ImGuiCol_Text];
+    if (egypt && oper == '/') {
+        float w1 = style.Colors[ImGuiCol_Button].w;
+        float w2 = style.Colors[ImGuiCol_Text].w;
+        style.Colors[ImGuiCol_Button] = style.Colors[ImGuiCol_Button] * 0.7f;
+        style.Colors[ImGuiCol_Text] = style.Colors[ImGuiCol_Text] * 0.6f;
+
+        style.Colors[ImGuiCol_Button].w = w1;
+        style.Colors[ImGuiCol_Text].w = w2;
+    }
+    if (ImGui::Button(std::string(1, oper).c_str(), ImVec2(dpi / 2, dpi / 2)) && !(egypt && oper == '/')) {
         if (ops.find(output.back()) != ops.end()) {
             output.back() = oper;
         }
@@ -189,6 +253,8 @@ void operatorButton(std::string& output, char oper, bool& op, bool& commaAlready
             output += oper;
         }
     }
+    style.Colors[ImGuiCol_Button] = color1;
+    style.Colors[ImGuiCol_Text] = color2;
 }
 
 
@@ -220,6 +286,7 @@ int main(int, char**)
         return 1;
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
+
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
@@ -269,31 +336,44 @@ int main(int, char**)
         glfwGetWindowSize(window, &tmp1, &tmp2);
         ImVec2 wSize(tmp1, tmp2);
 
-        ImGui::SetNextWindowSize({ 70000.f / dpi, 70000.f / dpi });
+        ImGui::SetNextWindowSize({ 5.f * dpi, 5.f * dpi });
         ImGui::Begin("Calculator");
-        ImGui::SetWindowFontScale(250.f / dpi);
+        ImGui::SetWindowFontScale(dpi / 50.);
 
-        ImGui::SetNextItemWidth(40000.f / dpi);
+        ImGui::SetNextItemWidth(2.8f * dpi);
         InputTextWithHint("Calculations", "", output, ImGuiInputTextFlags_ReadOnly);
 
-        if (ImGui::Button("Cl", ImVec2(9000 / dpi, 9000 / dpi))) {
+        if (ImGui::Button("Cl", ImVec2(dpi / 2, dpi / 2))) {
             output = "0";
 			commaAlready = false;
 			op = false;
         } ImGui::SameLine();
-        if (ImGui::Button("NS", ImVec2(9000 / dpi, 9000 / dpi))) {
+        if (ImGui::Button("NS", ImVec2(dpi / 2, dpi / 2))) {
             baseTmp = base;
             openNumSysWindow = true;
         } ImGui::SameLine();
-        if (ImGui::Button("Ac", ImVec2(9000 / dpi, 9000 / dpi))) {
+        if (ImGui::Button("Ac", ImVec2(dpi / 2, dpi / 2))) {
             accuracyTmp = accuracy;
             openAccuracyWindow = true;
 		} ImGui::SameLine();
-        if (ImGui::Button(".", ImVec2(9000 / dpi, 9000 / dpi)) && !commaAlready) {
+        ImVec4 color1 = style.Colors[ImGuiCol_Button];
+        ImVec4 color2 = style.Colors[ImGuiCol_Text];
+        if (egypt) {
+            float w1 = style.Colors[ImGuiCol_Button].w;
+            float w2 = style.Colors[ImGuiCol_Text].w;
+            style.Colors[ImGuiCol_Button] = style.Colors[ImGuiCol_Button] * 0.7f;
+            style.Colors[ImGuiCol_Text] = style.Colors[ImGuiCol_Text] * 0.6f;
+
+            style.Colors[ImGuiCol_Button].w = w1;
+            style.Colors[ImGuiCol_Text].w = w2;
+        }
+        if (ImGui::Button(".", ImVec2(dpi / 2, dpi / 2)) && !commaAlready) {
             output += '.';
             commaAlready = true;
         } ImGui::SameLine();
-        if (ImGui::Button("=", ImVec2(9000 / dpi, 9000 / dpi)) && ops.find(output.back()) == ops.end() && op) {
+        style.Colors[ImGuiCol_Button] = color1;
+        style.Colors[ImGuiCol_Text] = color2;
+        if (ImGui::Button("=", ImVec2(dpi / 2, dpi / 2)) && ops.find(output.back()) == ops.end() && op) {
             eval(output, base, accuracy);
             op = false;
             commaAlready = false;
@@ -319,24 +399,28 @@ int main(int, char**)
         ImGui::SameLine(); digitButton(output, '2', wSize, base, style);
         ImGui::SameLine(); digitButton(output, '3', wSize, base, style);
         
-		operatorButton(output, '+', op, commaAlready, base, accuracy, wSize); ImGui::SameLine();
-        operatorButton(output, '-', op, commaAlready, base, accuracy, wSize); ImGui::SameLine();
-        operatorButton(output, '*', op, commaAlready, base, accuracy, wSize); ImGui::SameLine();
-        operatorButton(output, '/', op, commaAlready, base, accuracy, wSize);
+		operatorButton(output, '+', op, commaAlready, base, accuracy, wSize, style); ImGui::SameLine();
+        operatorButton(output, '-', op, commaAlready, base, accuracy, wSize, style); ImGui::SameLine();
+        operatorButton(output, '*', op, commaAlready, base, accuracy, wSize, style); ImGui::SameLine();
+        operatorButton(output, '/', op, commaAlready, base, accuracy, wSize, style);
 
 
         if (openNumSysWindow) {
             ImGui::Begin("Numerical system");
-            ImGui::SetWindowFontScale(250.f / dpi);
+            ImGui::SetWindowFontScale(dpi / 50.);
 
             ImGui::Text("Choose the base (from 2 to 16 only)");
-            ImGui::InputInt("Base", &baseTmp, 1, 4);
+            ImGui::InputInt("Base", &baseTmp, 1, 4); ImGui::SameLine();
+            ImGui::Checkbox("Egyptian NS", &egypt);
             if (ImGui::Button("Cancel")) {
                 openNumSysWindow = false;
+                egypt = false;
             }
             if (ImGui::Button("Confirm") && baseTmp >= 2 && baseTmp <= 16) {
                 openNumSysWindow = false;
                 base = baseTmp;
+                op = false;
+                commaAlready = false;
                 output = "0";
             }
 
@@ -345,7 +429,7 @@ int main(int, char**)
 
         if (openAccuracyWindow) {
             ImGui::Begin("Accuracy");
-            ImGui::SetWindowFontScale(250.f / dpi);
+            ImGui::SetWindowFontScale(dpi / 50.);
 
             ImGui::Text("Number of decimal places (from 0 to 5 only)");
             ImGui::InputInt("Accuracy", &accuracyTmp, 1, 1);
@@ -355,6 +439,8 @@ int main(int, char**)
             if (ImGui::Button("Confirm") && accuracyTmp >= 0 && accuracyTmp <= 5) {
                 openAccuracyWindow = false;
                 accuracy = accuracyTmp;
+                op = false;
+                commaAlready = false;
                 output = "0";
             }
 
